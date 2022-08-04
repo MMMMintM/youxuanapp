ea2b1015d91344b2a36d552b64ddd953114f5544
MMMMintM <1962849542@qq.com>
2022-08-04 20:32:29 +0800

    commit

--- src/main/java/cn/edu/guet/youxuanapp/service/SysMenuService.java
+++ src/main/java/cn/edu/guet/youxuanapp/service/SysMenuService.java
@@ -23,7 +23,6 @@ public interface SysMenuService {
     /**
      * 查询菜单树,用户ID和用户名为空则查询全部
      *
-     * @param menuType 获取菜单类型，0：获取所有菜单，包含按钮，1：获取所有菜单，不包含按钮
      * @param userName
      * @return
      */
